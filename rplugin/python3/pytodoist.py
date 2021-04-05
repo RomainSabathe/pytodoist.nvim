339d359325d01ab3a4984fdec1524283bac9f002
Romain Sabathe <RSabathe@gmail.com>
2021-04-05 10:54:04 +0100

    Basic child/unchild functionality. Lots of bugs.

--- rplugin/python3/pytodoist.py
+++ rplugin/python3/pytodoist.py
@@ -1,8 +1,35 @@
 import os
+from typing import List
 from copy import copy, deepcopy
+from dataclasses import dataclass
 
 import neovim
 import todoist
+
+
+class TasksWorld:
+    def __init__(self, tasks: List[todoist.models.Item] = None):
+        self.tasks = []
+        self.id_to_task = {}
+
+        if tasks:
+            for task in tasks:
+                self.append(task)
+
+    def append(self, task: todoist.models.Item):
+        pass
+
+
+@dataclass
+class TaskInfo:
+    task: todoist.models.Item
+    indentation: int = 0
+
+    @property
+    def id(self):
+        return self.task["id"]
+
+NULL = 'null'
 
 
 @neovim.plugin
@@ -25,14 +52,14 @@ class Main(object):
     @neovim.autocmd("InsertEnter", sync=False)
     def register_current_line(self):
         if self.is_active:
-            line = self.nvim.current.line
+            line = self.nvim.current.line.strip()
             self.current_task = self.tasks[line] if line else None
             # self.current_task = self.tasks[self._get_current_line_index() - 1]
 
     @neovim.autocmd("InsertLeave", sync=False)
     def register_updated_line(self):
         if self.is_active:
-            new_task_content = self.nvim.current.line
+            new_task_content = self.nvim.current.line.strip()
             if self.current_task is not None:
                 updated_task = self._update_task(self.current_task, new_task_content)
             else:
@@ -78,28 +105,43 @@ class Main(object):
 
     # TODO: implement a NamedTuple as history items.
 
-    def _update_task(self, task, new_content: str, update_history: bool = True):
+    def _update_task(
+        self,
+        task,
+        new_content: str = None,
+        parent_id: int = None,
+        update_history: bool = True,
+    ):
         task_id = task["id"]
         old_task_content = task["content"]
 
-        # Updating the buffer `self.tasks`
-        updated_task = deepcopy(task)
-        updated_task["content"] = new_content
-        if update_history:
-            self._history_append(
-                prev_state=deepcopy(task), new_state=deepcopy(updated_task)
-            )
+        # Changing the 'content'
+        if new_content:
+            # Updating the buffer `self.tasks`
+            updated_task = deepcopy(task)
+            updated_task["content"] = new_content
+            if update_history:
+                self._history_append(
+                    prev_state=deepcopy(task), new_state=deepcopy(updated_task)
+                )
 
-        del self.tasks[old_task_content]
-        self.tasks[new_content] = deepcopy(updated_task)
+            del self.tasks[old_task_content]
+            self.tasks[new_content] = deepcopy(updated_task)
 
-        # Updating Todoist.
-        # self.nvim.command(f"""echo "About to replace\n{task['content']}\nwith\n{new_content}" """)
-        self.todoist.items.update(task_id, content=new_content)
-        self.todoist.commit()
-        # self.nvim.command(f'echo "Old line was: {self.old_line}. New line is: {self.new_line}"')
+            # Updating Todoist.
+            # self.nvim.command(f"""echo "About to replace\n{task['content']}\nwith\n{new_content}" """)
+            self.todoist.items.update(task_id, content=new_content)
+            self.todoist.commit()
+            # self.nvim.command(f'echo "Old line was: {self.old_line}. New line is: {self.new_line}"')
 
-        return updated_task
+        # Changing the 'parent'
+        if parent_id:
+            if parent_id == NULL:
+                parent_id = None
+            self.todoist.items.move(task_id, parent_id=parent_id)
+            self.todoist.commit()
+
+        # return updated_task
 
     @neovim.function("LoadTasks")
     def load_tasks(self, args):
@@ -117,6 +159,9 @@ class Main(object):
                     self.nvim.current.line = line
                     is_first_line = False
                 else:
+                    if item["parent_id"]:
+                        # This task has a parent task. It must be indented.
+                        line = f"   {line}"
                     self.nvim.current.buffer.append(line)
                 self.tasks[item["content"]] = item
 
@@ -130,7 +175,7 @@ class Main(object):
     def delete_task(self, args):
         if self.is_active:
             line = self.nvim.current.line
-            self._delete_task(line)
+            self._delete_task(line.strip())
             self.nvim.command("d")
         else:
             self.nvim.command(
@@ -147,6 +192,35 @@ class Main(object):
             self.nvim.command(
                 'echo "The env var DEBUG_TODOIST is not set. Not doing anything."'
             )
+
+    @neovim.function("MakeChild")
+    def make_child(self, args):
+        line = self.nvim.current.line
+        current_task = self.tasks[line.strip()]
+
+        idx = self._get_current_line_index() - 1
+        parent_line = self.nvim.current.buffer[idx - 1]
+        parent_task = self.tasks[parent_line.strip()]
+
+        self.nvim.command(">1")
+        self._update_task(current_task, parent_id=parent_task["id"])
+
+    @neovim.function("UnmakeChild")
+    def unmake_child(self, args):
+        line = self.nvim.current.line
+        current_task = self.tasks[line.strip()]
+
+        parent_id = current_task["parent_id"]
+        parent_tasks = [task for task in self.tasks.values() if task["id"] == parent_id]
+        parent_task = parent_tasks[0]  # There should only be one anyway.
+        grand_father_id = parent_task['parent_id']
+        if grand_father_id is None:
+            # In this lib, None is reserved for no actions. However, having a 'parent_id'
+            # that equals to None is a meaningful information. We interpret it as NULL.
+            grand_father_id = NULL
+
+        self.nvim.command("<1")
+        self._update_task(current_task, parent_id=grand_father_id)
 
     @neovim.function("Undo")
     def undo(self, args):
